d3ce125ecb38a628ec79e1d7264ae804990bce16
ColdVolcano <mineflamecraft@hotmail.com>
2017-06-17 18:55:13 -0500

    >.>

--- VitaruInstall/Properties/AssemblyInfo.cs
+++ VitaruInstall/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VitaruInstall")]
+[assembly: AssemblyTitle("Vitaru Installer")]
 [assembly: AssemblyDescription("Install or uninstall Vitaru!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Symcol")]
-[assembly: AssemblyProduct("VitaruInstall")]
+[assembly: AssemblyProduct("Vitaru Installer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.1.0")]
+[assembly: AssemblyFileVersion("0.3.1.0")]
