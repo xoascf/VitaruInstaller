dcd6874be2ff43b3d6b788f425c644f57d276d15
Shawdooow <banhammer1915@gmail.com>
2017-07-21 10:47:44 -0400

    Make better

--- VitaruInstall/Program.cs
+++ VitaruInstall/Program.cs
@@ -8,17 +8,25 @@ using SQLite.Net;
 using SQLite.Net.Platform.Win32;
 using SQLite.Net.Attributes;
 
-namespace VitaruInstall
+namespace RulesetInstall
 {
     class Program
     {
-        private const string vitaruDllName = @"osu.Game.Rulesets.Vitaru.dll";
-        private const string vitaruVersion = "0.4.1";
-        private const string databaseName = "client.db";
+        /// <summary>
+        /// In order to change this to do your gamemode, follow our structure and add a release on github. Then, change RulesetName, RulesetDllName, RulesetVersion and RulesetInstantiationInfo to match your gamemode's information.
+        /// </summary>
+        public const string RulesetName = "vitaru";
+        public const string RulesetDllName = @"osu.Game.Rulesets.Vitaru.dll";
+        public const string RulesetVersion = "0.4.1";
+        public const string RulesetInstantiationInfo = "osu.Game.Rulesets.Vitaru.VitaruRuleset, osu.Game.Rulesets.Vitaru, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+
+        public const string DatabaseName = "client.db";
+
         static void Main(string[] args)
         {
             string databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "osu");
             string lazerPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "osulazer");
+
             if (Directory.Exists(lazerPath) && Directory.GetDirectories(lazerPath).Length > 0)
             {
                 List<string> wew = new List<string>();
@@ -26,27 +34,32 @@ namespace VitaruInstall
                 List<int> yearOfOsuVersion = new List<int>();
                 List<int> monthDateOfOsuVersion = new List<int>();
                 List<int> discrimOfOsuVersion = new List<int>();
+
                 for (int i = 0; i < wew.Count; i++)
                 {
                     string s = wew[i].Split('\\')[wew[i].Split('\\').Length - 1];
                     if (s.StartsWith("app-") && int.TryParse(s.Split('-')[1].Split('.')[0], out int result))
                         yearOfOsuVersion.Add(result);
                 }
+
                 for (int i = 0; i < wew.Count; i++)
                 {
                     string s = wew[i].Split('\\')[wew[i].Split('\\').Length - 1];
                     if (s.StartsWith("app-") && int.TryParse(s.Split('-')[1].Split('.')[1], out int result))
                         monthDateOfOsuVersion.Add(result);
                 }
+
                 for (int i = 0; i < wew.Count; i++)
                 {
                     string s = wew[i].Split('\\')[wew[i].Split('\\').Length - 1];
                     if (s.StartsWith("app-") && int.TryParse(s.Split('-')[1].Split('.')[2], out int result))
                         discrimOfOsuVersion.Add(result);
                 }
+
                 int year = 0;
                 int monthDate = 0;
                 int discrim = 0;
+
                 for (int i = 0; i < yearOfOsuVersion.Count; i++)
                 {
                     if (yearOfOsuVersion[i] >= year)
@@ -61,10 +74,11 @@ namespace VitaruInstall
                         }
                     }
                 }
-                if (year > 2015 && monthDate > 100 && discrim >= 0 && Directory.Exists(databasePath) && File.Exists(Path.Combine(databasePath, databaseName)))
+
+                if (year > 2015 && monthDate > 100 && discrim >= 0 && Directory.Exists(databasePath) && File.Exists(Path.Combine(databasePath, DatabaseName)))
                 {
                     string osuVer = "app-" + year + "." + monthDate + "." + discrim;
-                    if (File.Exists(Path.Combine(lazerPath, osuVer, vitaruDllName)))
+                    if (File.Exists(Path.Combine(lazerPath, osuVer, RulesetDllName)))
                     {
                         string what = "";
                         do
@@ -80,42 +94,49 @@ namespace VitaruInstall
                         else
                             Install(lazerPath, osuVer);
                     }
+
                     else
                     {
                         Install(lazerPath, osuVer);
                     }
                 }
+
                 else
                 {
-                    Console.WriteLine("It seems you don't have osu!lazer installed, please go do that before installing vitaru");
+                    Console.WriteLine("It seems you don't have osu!lazer installed, please go do that before installing " + RulesetName);
                     Console.ReadKey();
                 }
             }
+
             else
             {
-                Console.WriteLine("It seems you don't have osu!lazer installed, please go do that before installing vitaru");
+                Console.WriteLine("It seems you don't have osu!lazer installed, please go do that before installing " + RulesetName);
                 Console.ReadKey();
             }
         }
 
         static void Install(string lazerPath, string osuVer)
         {
-            string vitaruPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "vitaru");
-            string vitaruFile = Path.Combine(vitaruPath, vitaruDllName);
-            string focusPath = Path.Combine(lazerPath, osuVer, vitaruDllName);
+            string vitaruPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), RulesetName);
+            string vitaruFile = Path.Combine(vitaruPath, RulesetDllName);
+            string focusPath = Path.Combine(lazerPath, osuVer, RulesetDllName);
+
             if (!(Process.GetProcessesByName("osu!").FirstOrDefault(p => p.MainModule.FileName.StartsWith(lazerPath)) != default(Process)))
             {
-                Console.WriteLine("Downloading latest vitaru...");
+                Console.WriteLine("Downloading latest " + RulesetName + "...");
+
                 if (!Directory.Exists(vitaruPath))
                     Directory.CreateDirectory(vitaruPath);
                 if (File.Exists(vitaruFile))
                     File.Delete(vitaruFile);
-                string dllLink = "https://github.com/Symcol/osu/releases/download/" + vitaruVersion + "/osu.Game.Rulesets.Vitaru.dll";
+
+                string dllLink = "https://github.com/Symcol/osu/releases/download/" + RulesetVersion + "/" + RulesetDllName;
                 using (WebClient client = new WebClient())
                     client.DownloadFile(dllLink, vitaruFile);
-                Console.WriteLine("Succesfully downloaded vitaru ver {0}!", vitaruVersion);
+                Console.WriteLine("Succesfully downloaded " + RulesetName + " ver {0}!", RulesetVersion);
                 Console.WriteLine("Installing to latest osu!...");
                 Console.WriteLine("Installing to version {0}", osuVer);
+
                 if (File.Exists(focusPath))
                 {
                     File.Delete(focusPath);
@@ -123,30 +144,33 @@ namespace VitaruInstall
                 }
                 else
                     File.Copy(vitaruFile, focusPath);
+
                 Console.WriteLine("Adding to database");
-                SQLiteConnection connection = new SQLiteConnection(new SQLitePlatformWin32($@"{Environment.CurrentDirectory}/{(IntPtr.Size == 8 ? "x64" : "x86")}"), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "osu", databaseName));
+                SQLiteConnection connection = new SQLiteConnection(new SQLitePlatformWin32($@"{Environment.CurrentDirectory}/{(IntPtr.Size == 8 ? "x64" : "x86")}"), Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "osu", DatabaseName));
                 connection.BeginTransaction();
                 connection.InsertOrReplace(new RulesetInfo());
                 connection.Close();
-                Console.WriteLine("Succesfully installed Vitaru to osu!lazer version {0}", osuVer);
+                Console.WriteLine("Succesfully installed " + RulesetName + " to osu!lazer version {0}", osuVer);
             }
+
             else
-                Console.WriteLine("It seems osu!lazer is running! Close it to install/reinstall vitaru!");
+                Console.WriteLine("It seems osu!lazer is running! Close it to install/reinstall " + RulesetName + "!");
             Console.ReadKey();
         }
+
         static void Uninstall(string lazerPath, string osuVer)
         {
-            string vitaruDllPath = Path.Combine(lazerPath, osuVer, vitaruDllName);
-            Console.WriteLine("Will uninstall Vitaru from osu!lazer version {0}", osuVer);
+            string vitaruDllPath = Path.Combine(lazerPath, osuVer, RulesetDllName);
+            Console.WriteLine("Will uninstall " + RulesetName + " from osu!lazer version {0}", osuVer);
             if (File.Exists(vitaruDllPath))
             {
                 if (!(Process.GetProcessesByName("osu!").FirstOrDefault(p => p.MainModule.FileName.StartsWith(lazerPath)) != default(Process)))
                 {
                     File.Delete(vitaruDllPath);
-                    Console.WriteLine("Uninstalled Vitaru, hope you liked it :)");
+                    Console.WriteLine("Uninstalled " + RulesetName + ", hope you liked it :)");
                 }
                 else
-                    Console.WriteLine("It seems osu!lazer is running! Close it to uninstall vitaru");
+                    Console.WriteLine("It seems osu!lazer is running! Close it to uninstall " + RulesetName);
             }
             else
                 Console.WriteLine("The dll was deleted before I could do something...");
@@ -160,10 +184,10 @@ namespace VitaruInstall
         public int? ID { get; set; }
 
         [Indexed(Unique = true)]
-        public string Name => "vitaru!";
+        public string Name => Program.RulesetName;
 
         [Indexed(Unique = true)]
-        public string InstantiationInfo => "osu.Game.Rulesets.Vitaru.VitaruRuleset, osu.Game.Rulesets.Vitaru, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+        public string InstantiationInfo => Program.RulesetInstantiationInfo;
 
         [Indexed]
         public bool Available => true;
